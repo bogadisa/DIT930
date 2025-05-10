89f590fddd1aefa91179377ab6828be98d65b1b9
Bogadisa <magnus@hoddevik.eu>
2025-05-10 10:30:49 +0200

    Submitted assignment 3

--- assignment_3/rdfs_and_csv_to_triples.py
+++ assignment_3/rdfs_and_csv_to_triples.py
@@ -1,3 +1,5 @@
+# Group 38
+# Haoqi Yu, Bjørn Magnus Hoddevik
 from rdfpandas.graph import to_dataframe
 import pandas as pd
 from rdflib import Graph, URIRef, RDF, Literal, BNode
@@ -44,10 +46,15 @@ def get_subset_df(entity_name: str):
                 f":{entity_name}",
             ),
             subset_column_match(
-                objects_df, r"rdfs:domain{URIRef}[0]", r"rdfs:domain{URIRef}[1]", f":{entity_name}"
+                objects_df,
+                r"rdfs:domain{URIRef}[0]",
+                r"rdfs:domain{URIRef}[1]",
+                f":{entity_name}",
             ),
         )
     )
+
+
 # print(student_df.columns)
 # print(student_df)
 # quit()
@@ -55,6 +62,7 @@ def get_subset_df(entity_name: str):
 
 g_students = Graph()
 default = Namespace("http://example.org/")
+
 
 def convert_to_correct_type(value: str, value_type: str):
     if "boolean" in value_type:
@@ -70,31 +78,35 @@ def convert_to_correct_type(value: str, value_type: str):
             # return None
             return value
 
+
 full_graph = Graph()
-def df_to_rdf(csv_data: pd.DataFrame, rdf_schema: pd.DataFrame, key_column: str, entity_name: str, is_bnode: bool):
+
+
+def df_to_rdf(
+    csv_data: pd.DataFrame,
+    rdf_schema: pd.DataFrame,
+    key_column: str,
+    entity_name: str,
+    is_bnode: bool,
+):
     for _, row in csv_data.iterrows():
         id = URIRef(row[key_column], default)
         if is_bnode:
             bnode = BNode()
-            full_graph.add((
-                id,
-                URIRef(entity_name, default),
-                bnode
-            ))
+            full_graph.add((id, URIRef(entity_name, default), bnode))
             id = bnode
         else:
-            full_graph.add(
-                (
-                    id,
-                    RDF.type,
-                    URIRef(entity_name, default)
-                )
-            )
+            full_graph.add((id, RDF.type, URIRef(entity_name, default)))
         for index, rdf_row in rdf_schema.iterrows():
-            if rdf_row[r"rdf:type{URIRef}"] in ("owl:DatatypeProperty", "owl:ObjectProperty"):
-            # if rdf_row[r"rdf:type{URIRef}"] == "owl:DatatypeProperty":
+            if rdf_row[r"rdf:type{URIRef}"] in (
+                "owl:DatatypeProperty",
+                "owl:ObjectProperty",
+            ):
+                # if rdf_row[r"rdf:type{URIRef}"] == "owl:DatatypeProperty":
                 index = index.replace(":", "")
-                if index == key_column or (is_bnode and rdf_row[r"rdf:type{URIRef}"] == "owl:ObjectProperty"):
+                if index == key_column or (
+                    is_bnode and rdf_row[r"rdf:type{URIRef}"] == "owl:ObjectProperty"
+                ):
                     continue
                 if index in csv_data.columns:
                     full_graph.add(
@@ -102,8 +114,11 @@ def df_to_rdf(csv_data: pd.DataFrame, rdf_schema: pd.DataFrame, key_column: str,
                             id,
                             URIRef(index, default),
                             Literal(
-                                convert_to_correct_type(row[index], rdf_row[r"rdfs:range{URIRef}"]) 
-                                if rdf_row[r"rdf:type{URIRef}"] == "owl:DatatypeProperty"
+                                convert_to_correct_type(
+                                    row[index], rdf_row[r"rdfs:range{URIRef}"]
+                                )
+                                if rdf_row[r"rdf:type{URIRef}"]
+                                == "owl:DatatypeProperty"
                                 else row[index]
                             ),
                             # Literal(convert_to_correct_type(row[index], rdf_row[r"rdfs:range{URIRef}"]))
@@ -114,31 +129,44 @@ def df_to_rdf(csv_data: pd.DataFrame, rdf_schema: pd.DataFrame, key_column: str,
         # for s, p, o in g.triples((None, None, None)):
         #     print(f"{s} - {p}, {o}")
 
-def add_csv_to_graph(filepath: str, column_rename: dict[str, str], key: str, name: str, entity_name: str = None, is_bnode: bool = False):
+
+def add_csv_to_graph(
+    filepath: str,
+    column_rename: dict[str, str],
+    key: str,
+    name: str,
+    entity_name: str = None,
+    is_bnode: bool = False,
+):
     if entity_name is None:
         entity_name = name
     data_df = pd.read_csv(filepath, dtype=str)
     data_df = data_df.rename(columns=column_rename)
     if key not in data_df.columns:
         if key == "programmeInstanceID":
-            data_df["programmeInstanceID"] = data_df["programmeInstanceProgrammeCode"] + "-" + data_df["programmeInstanceStudyYear"]
+            data_df["programmeInstanceID"] = (
+                data_df["programmeInstanceProgrammeCode"]
+                + "-"
+                + data_df["programmeInstanceStudyYear"]
+            )
 
     rdf_df = get_subset_df(name)
     df_to_rdf(data_df, rdf_df, key, entity_name, is_bnode)
 
+
 student_rename = {
-    "Student name" : "studentName",
-    "Student id" : "studentID",
-    "Programme" : "BelongsToProgram",
-    "Year" : "studentYear",
-    "Graduated" : "studentGraduated",
+    "Student name": "studentName",
+    "Student id": "studentID",
+    "Programme": "BelongsToProgram",
+    "Year": "studentYear",
+    "Graduated": "studentGraduated",
 }
 add_csv_to_graph("data/Students.csv", student_rename, "studentID", "Student")
 program_rename = {
-    "Programme name" : "programmeName",
-    "Programme code" : "programmeCode",
-    "Department name" : "OwnedByDepartment",
-    "Director" : "programmeDirector",
+    "Programme name": "programmeName",
+    "Programme code": "programmeCode",
+    "Department name": "OwnedByDepartment",
+    "Director": "programmeDirector",
 }
 add_csv_to_graph("data/Programmes.csv", program_rename, "programmeCode", "Programme")
 
@@ -150,7 +178,13 @@ assigned_hours_rename = {
     "Hours": "teachesAssignedHours",
     "Course Instance": "teachesCourseOfferingInstanceID",
 }
-add_csv_to_graph("data/Assigned_Hours.csv", assigned_hours_rename, "teachesTeacherID", "Teaches", is_bnode=True)
+add_csv_to_graph(
+    "data/Assigned_Hours.csv",
+    assigned_hours_rename,
+    "teachesTeacherID",
+    "Teaches",
+    is_bnode=True,
+)
 
 course_instance_rename = {
     "Course code": "courseOfferingCourseCode",
@@ -159,7 +193,12 @@ course_instance_rename = {
     "Instance_id": "courseOfferingInstanceID",
     "Examiner": "Examiner",
 }
-add_csv_to_graph("data/Course_Instances.csv", course_instance_rename, "courseOfferingInstanceID", "CourseOffering")
+add_csv_to_graph(
+    "data/Course_Instances.csv",
+    course_instance_rename,
+    "courseOfferingInstanceID",
+    "CourseOffering",
+)
 
 course_plannings_rename = {
     "Course": "courseOfferingInstanceID",
@@ -167,7 +206,12 @@ course_plannings_rename = {
     "Senior Hours": "courseOfferingSeniorHours",
     "Assistant Hours": "courseOfferingAssistantHours",
 }
-add_csv_to_graph("data/Course_plannings.csv", course_plannings_rename, "courseOfferingInstanceID", "CourseOffering")
+add_csv_to_graph(
+    "data/Course_plannings.csv",
+    course_plannings_rename,
+    "courseOfferingInstanceID",
+    "CourseOffering",
+)
 
 courses_rename = {
     "Course name": "courseName",
@@ -184,7 +228,14 @@ courses_given_by_rename = {
     "Department": "divisionDepartmentID",
     "Division": "divisionID",
 }
-add_csv_to_graph("data/Courses.csv", courses_given_by_rename, "courseCode", "Division", "GivenBy", is_bnode=True)
+add_csv_to_graph(
+    "data/Courses.csv",
+    courses_given_by_rename,
+    "courseCode",
+    "Division",
+    "GivenBy",
+    is_bnode=True,
+)
 
 programme_courses_rename = {
     "Programme code": "programmeInstanceProgrammeCode",
@@ -193,7 +244,12 @@ programme_courses_rename = {
     # "Course": "programmeInstanceCourseCode",
     # "Course Type": "programmeInstanceCourseType",
 }
-add_csv_to_graph("data/Programme_Courses.csv", programme_courses_rename, "programmeInstanceProgrammeCode", "ProgrammeInstance")
+add_csv_to_graph(
+    "data/Programme_Courses.csv",
+    programme_courses_rename,
+    "programmeInstanceProgrammeCode",
+    "ProgrammeInstance",
+)
 programme_courses_rename = {
     "Programme code": "programmeInstanceProgrammeCode",
     # "Study Year": "programmeInstanceStudyYear",
@@ -201,7 +257,13 @@ programme_courses_rename = {
     "Course": "programmeCoursesCourseCode",
     "Course Type": "programmeCoursesCourseType",
 }
-add_csv_to_graph("data/Programme_Courses.csv", programme_courses_rename, "programmeInstanceProgrammeCode", "ProgrammeCourses", is_bnode=True)
+add_csv_to_graph(
+    "data/Programme_Courses.csv",
+    programme_courses_rename,
+    "programmeInstanceProgrammeCode",
+    "ProgrammeCourses",
+    is_bnode=True,
+)
 
 registrations_rename = {
     "Course Instance": "registrationsCourseOfferingInstanceID",
@@ -209,14 +271,26 @@ registrations_rename = {
     "Status": "registrationsStatus",
     "Grade": "registrationsGrade",
 }
-add_csv_to_graph("data/Registrations.csv", registrations_rename, "registrationsStudentID", "Registrations", is_bnode=True)
+add_csv_to_graph(
+    "data/Registrations.csv",
+    registrations_rename,
+    "registrationsStudentID",
+    "Registrations",
+    is_bnode=True,
+)
 
 reported_hours_rename = {
     "Course code": "teachesCourseCode",
     "Teacher Id": "teachesTeacherID",
     "Hours": "teachesReportedHours",
 }
-add_csv_to_graph("data/Reported_Hours.csv", reported_hours_rename, "teachesTeacherID", "Teaches", is_bnode=True)
+add_csv_to_graph(
+    "data/Reported_Hours.csv",
+    reported_hours_rename,
+    "teachesTeacherID",
+    "Teaches",
+    is_bnode=True,
+)
 
 senior_teachers_rename = {
     "Teacher name": "teacherName",
@@ -224,10 +298,22 @@ senior_teachers_rename = {
     "Department name": "teacherDepartmentName",
     "Division name": "teacherDivisionName",
 }
-add_csv_to_graph("data/Senior_Teachers.csv", senior_teachers_rename, "teacherID", "Teacher", "SeniorTeacher")
+add_csv_to_graph(
+    "data/Senior_Teachers.csv",
+    senior_teachers_rename,
+    "teacherID",
+    "Teacher",
+    "SeniorTeacher",
+)
 
 teaching_assistants_rename = senior_teachers_rename
-add_csv_to_graph("data/Teaching_Assistants.csv", teaching_assistants_rename, "teacherID", "Teacher", "TeachingAssistant")
+add_csv_to_graph(
+    "data/Teaching_Assistants.csv",
+    teaching_assistants_rename,
+    "teacherID",
+    "Teacher",
+    "TeachingAssistant",
+)
 
 
 full_graph.serialize("assignment_3/graph.txt", format="turtle")
